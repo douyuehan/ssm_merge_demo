8fda4ae3ebfd33748b8687a507761967fc81bf43
your name <your email>
2018-11-28 15:41:19 +0800

    transaction

--- src/com/neusoft/controller/HelloController.java
+++ src/com/neusoft/controller/HelloController.java
@@ -4,6 +4,7 @@ import com.neusoft.domain.Dept;
 import com.neusoft.mapper.DeptMapper;
 import com.neusoft.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,13 +14,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
+    @Value("${user_name}")
+    private String uname;
+
+
+
+    @Value("${chengmi_crawl_timer_enable}")
+    private Boolean bbb;
+
     @Autowired
     HelloService helloService;
 
     @RequestMapping("/hello")
     public String hello()
     {
-//        System.out.println("hello");
+        System.out.println(uname + bbb);
         Dept dept = helloService.getDeptByID(20);
         System.out.println(dept);
         return "hello";
--- src/com/neusoft/service/HelloServiceImpl.java
+++ src/com/neusoft/service/HelloServiceImpl.java
@@ -4,6 +4,7 @@ import com.neusoft.domain.Dept;
 import com.neusoft.mapper.DeptMapper;
 import com.neusoft.mapper.EmpMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
@@ -20,10 +21,14 @@ public class HelloServiceImpl implements HelloService {
     @Autowired
     EmpMapper empMapper;
 
+    @Value("${age}")
+    private Integer age;
+
     @Override
     @Transactional
     public Dept getDeptByID(int id) {
 
+        System.out.println("ageageage" + age);
         Dept dept = new Dept();
         dept.setDname("develop3");
         deptMapper.addDept(dept);
