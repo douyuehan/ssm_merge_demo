914fcd83e42d781040462116dc2a6de2bc173f65
your name <your email>
2018-11-28 14:59:47 +0800

    transaction

--- src/com/neusoft/service/HelloServiceImpl.java
+++ src/com/neusoft/service/HelloServiceImpl.java
@@ -2,10 +2,12 @@ package com.neusoft.service;
 
 import com.neusoft.domain.Dept;
 import com.neusoft.mapper.DeptMapper;
+import com.neusoft.mapper.EmpMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by Administrator on 2018/11/28.
@@ -15,8 +17,23 @@ public class HelloServiceImpl implements HelloService {
     @Autowired
     DeptMapper deptMapper;
 
+    @Autowired
+    EmpMapper empMapper;
+
     @Override
+    @Transactional
     public Dept getDeptByID(int id) {
+
+        Dept dept = new Dept();
+        dept.setDname("develop3");
+        deptMapper.addDept(dept);
+
+        int k = 3/0;
+
+        empMapper.deleteByPrimaryKey(7369);
+
         return deptMapper.getDeptByID(id);
+
+
     }
 }
